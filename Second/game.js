f54c13481697efea2217ce0236d6791acf09b0f5
Afieqha Mieza Azemi <afieqhaazemi@gmail.com>
2020-02-21 16:28:02 -0500

    destroying the bacteria by index

--- Second/game.js
+++ Second/game.js
@@ -1,15 +1,14 @@
-class Game
-{
+class Game {
     //-----------------------------------------------------
     // Method: constructor() 
     // Descritption: Init the game enviromend 
     //-----------------------------------------------------
-    constructor()
-    {
+    constructor() {
+        this.bactArr = [];
         //Creating a WebGL Context Canvas
         this.canvas = document.getElementById('gameSurface');
         this.gl = this.canvas.getContext('webgl');
-    
+
         // Vertex and fragement shader source
         var vertCode = [
             'attribute vec3 coordinates;',
@@ -18,7 +17,7 @@ class Game
             '	gl_Position = vec4(coordinates, 1.0);',
             '}'
         ].join('\n');
-    
+
         var fragCode = [
             'precision mediump float;',
             'uniform vec4 fColor;',
@@ -28,47 +27,47 @@ class Game
             ' gl_FragColor = fColor;',
             '}'
         ].join('\n');
-    
+
         // Create an empty buffer object
         var vertex_buffer = this.gl.createBuffer();
-    
+
         // Set the view port
         this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-    
+
         // Bind appropriate array buffer to it
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertex_buffer);
-    
+
         // Enable the depth test
         this.gl.enable(this.gl.DEPTH_TEST);
-    
+
         // Create vertex and fragment shader objects
         var vertShader = this.gl.createShader(this.gl.VERTEX_SHADER);
         var fragShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
-    
+
         // Shaders
         // Attach vertex shader source code and compile
         this.gl.shaderSource(vertShader, vertCode);
         this.gl.compileShader(vertShader);
-    
+
         // Attach fragment shader source code and compile
         this.gl.shaderSource(fragShader, fragCode);
         this.gl.compileShader(fragShader);
-    
+
         // Create shader program
         var shaderProgram = this.gl.createProgram();
-    
+
         // Attach the vertex and fragment shader
         this.gl.attachShader(shaderProgram, vertShader);
         this.gl.attachShader(shaderProgram, fragShader);
-    
+
         // Link and use
         this.gl.linkProgram(shaderProgram);
         this.gl.useProgram(shaderProgram);
-    
+
         // Get the attribute and uniform location
         var coord = this.gl.getAttribLocation(shaderProgram, "coordinates");
         this.fColor = this.gl.getUniformLocation(shaderProgram, "fColor");
-    
+
         // Point an attribute to the currently bound VBO and enable the attribute
         this.gl.vertexAttribPointer(coord, 3, this.gl.FLOAT, false, 0, 0);
         this.gl.enableVertexAttribArray(coord);
@@ -79,34 +78,26 @@ class Game
 
         //this.canvas.onmousedown = function(e, canvas){this.someFuntion};
         //document.getElementById("gameSurface").addEventListener("mousedown", mouseDown);
-        
+
         function mouseDown() {
             console.log('someFuntion');
         }
-
-        
-        
-    
     }
 
-   
+
     //-----------------------------------------------------
     // Method: start() 
     // Descritption: Start the game
     //-----------------------------------------------------
-    start()
-    {
-        
+    start() {
+
         // Use default disk position and colour
         this.disk = new Disk();
-        
-        this.bactArr = [];
-                
+
         this.bactArr.push(new Bact(this.disk));
-        //*
 
         // Creating bact array
-        for (let i = 0; i < 9; ) {
+        for (let i = 0; i < 9;) {
             var collFlag = false;
             var tempBact = new Bact(this.disk);
 
@@ -125,59 +116,68 @@ class Game
         }
 
         for (let i = 0; i < this.bactArr.length; i++) {
-            this.bactArr[i].draw(this.gl,this.fColor);
+            this.bactArr[i].draw(this.gl, this.fColor);
         }
-        
 
-        this.disk.draw(this.gl,this.fColor);
+        this.disk.draw(this.gl, this.fColor);
+
+
 
         //this.g_updateCallbackarray = []
         //this.g_frameCount = 0;
         this.g_updateEventListenerEnabled = false
 
         //-----------------------------------------------------
-        //*/
     } // End start()
 
     // Test Method for drawing circles
-    drawCircle(_x, _y, _r, _color) 
-    {
+    drawCircle(_x, _y, _r, _color) {
         // For storing the produces vertices
         var vertices = [];
-    
+
         // Prepare vertices
         for (let i = 1; i <= 360; i++) {
             var y1 = _r * Math.sin(i) + _y;
             var x1 = _r * Math.cos(i) + _x;
-    
+
             var y2 = _r * Math.sin(i + 1) + _y;
             var x2 = _r * Math.cos(i + 1) + _x;
-    
+
             vertices.push(_x);
             vertices.push(_y);
             vertices.push(0);
-    
+
             vertices.push(x1);
             vertices.push(y1);
             vertices.push(0);
-    
+
             vertices.push(x2);
             vertices.push(y2);
             vertices.push(0);
         }
-    
+
         // Pass the vertex data to the buffer
         this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
-    
+
         // Pass color data to uniform fColor
         this.gl.uniform4f(this.fColor, _color[0], _color[1], _color[2], _color[3]);
-    
+
         // Drawing triangles
         this.gl.clearColor(0, 1, 0, 0.9);
         // Draw the triangle 360*3, 3 layers of vertices (disk)
         this.gl.drawArrays(this.gl.TRIANGLES, 0, 360 * 3);
     } // End draw
 
-  
+    //destroy the bacteria
+    destroy(index) {
 
+        this.r = 0;
+        this.x = 0;
+        this.y = 0;
+        this.alive = false;
+
+        // Remove destroyed bacteria from the bacteria array
+        this.bactArr.splice(index, 1);
+
+    }
 }
