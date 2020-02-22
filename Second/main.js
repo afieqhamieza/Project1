edd14841db539f9d2afd47eb8c344eb1a5b6e85c
jbledua <jbledua@gmail.com>
2020-02-21 22:27:16 -0500

    Something

--- Second/main.js
+++ Second/main.js
@@ -53,56 +53,10 @@ function g_frameEventHndlr() {
     window.requestAnimationFrame(g_frameEventHndlr);
 }
 
-<<<<<<< HEAD
 //---------------------------------------------------------
-=======
-function g_clickEventHndlr(_x, _y) {
-    if (!mainGameObj.win()) {
-        //something = something + 1;
-        console.log(_x, _y)
-
-        mainGameObj.destroy(0);
-
-        /*
-    for (let i= 0; i < 5; i++) 
-    {
-        if(mainGameObj.bactArr[i].clicked(_x,_y) == true)
-        {
-            console.log(i, "was clicked");
-        }
-        else
-        {
-            console.log(i, "was not clicked");
-        }
-        
-        //const element = array[index];  
-    }
-    //*/
-
-        //mainGameObj.update();
-
-        //window.requestAnimationFrame(g_frameEventHndlr);
-
-    }
-}
-/*
-document.getElementById("gameSurface").addEventListener("mousedown", mouseDown);
-document.getElementById("gameSurface").addEventListener("mouseup", mouseUp);
-
-function mouseDown() {
-  document.getElementById("demo").innerHTML = "The mouse button is held down.";
-}
-
-function mouseUp() {
-  document.getElementById("demo").innerHTML = "You released the mouse button.";
-}
-//*/
-// -------------------------------------------------------
->>>>>>> fe0210e0521184e3dcf5a1bf6dcfad2facbcbbc9
 // Click
 //---------------------------------------------------------
 
-<<<<<<< HEAD
 function g_clickEventHndlr(_x,_y)
 {
     var tempArr = mainGameObj.getBactArr()
@@ -115,73 +69,3 @@ function g_clickEventHndlr(_x,_y)
        
     }
 }
-=======
-g_clickEventHndlr(_x,_y)
-{
-    console.log(_x,_y);
-}
-//*/
-
-
-// -------------------------------------------------------
-// Grow
-//----------------------------------------------------------
-
-/*
-
-var g_updateCallbackarray = []
-var g_frameCount = 0;
-var g_updateEventListenerEnabled = false
-
-function toggleUpdateEventListener()
-{
-    if (g_updateEventListenerEnabled)
-    {
-        g_updateEventListenerEnabled = false;
-    }
-    else
-    {
-        g_updateEventListenerEnabled = true;
-        requestAnimationFrame(updateEventHndlr);
-    }
-
-    //console.log("updateEventListener " , g_updateEventListenerEnabled)
-}
-function updateEventListener()
-{
-    if (g_updateEventListenerEnabled) requestAnimationFrame(updateEventHndlr);
-}
-
-function addUpdateCallback(_callback)
-{
-    g_updateCallbackarray.push(_callback);
-}
-
-function removeUpdateCallback(_callback)
-{
-
-}
-
-function updateEventHndlr()
-{
-    // Increament Frame Count
-    //g_frameCount++;
-
-    //console.log(g_frameCount);
-    for (let index = 0; index < bactArr.length; index++) {
-        bactArr[index].update()
-
-    }
-
-
-    bactArr.forEach(element => {
-        element.update()
-    });
-
-    if (g_updateEventListenerEnabled) requestAnimationFrame(updateEventHndlr);
-
-    //updateEventListener();
-}
-
-//*/
->>>>>>> fe0210e0521184e3dcf5a1bf6dcfad2facbcbbc9
